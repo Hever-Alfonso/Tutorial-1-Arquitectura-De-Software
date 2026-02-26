7c0a9700cdc93c6fa7c254d16a70cca28ab0c4fe
Hever Alfonso <haalfonsoj@eafit.edu.co>
2026-02-26 17:07:22 -0500

    feat(pages): register ImageStorage provider in PagesConfig.ready

--- helloworld/pages/apps.py
+++ helloworld/pages/apps.py
@@ -1,5 +1,26 @@
 from django.apps import AppConfig
+from django.utils.module_loading import import_string
+from django.conf import settings
 
 
 class PagesConfig(AppConfig):
+    """
+    Configuracion de la aplicacion 'pages'.
+
+    El metodo ready() actua como service provider:
+    al arrancar Django, importa la clase de storage configurada
+    en settings.IMAGE_STORAGE_CLASS. Esto valida que la clase
+    existe y esta correctamente configurada antes de recibir requests.
+
+    Esto es parte del patron DIP: la aplicacion sabe que hay un
+    ImageStorage, pero no cual implementacion concreta se usara
+    hasta leer la configuracion.
+    """
+
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
+
+    def ready(self):
+        # Carga la clase de storage al inicio de la aplicacion
+        # Si IMAGE_STORAGE_CLASS apunta a una clase inexistente, falla rapido
+        import_string(settings.IMAGE_STORAGE_CLASS)
